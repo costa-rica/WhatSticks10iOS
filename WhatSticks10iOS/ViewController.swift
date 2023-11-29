f0d6f1601ed0c7661da97c2c7da05ecc3f869d5c
Nick Rodriguez <nrodrig1@gmail.com>
2023-11-29 08:53:41 +0100

    Initial Commit

---
+++ WhatSticks10iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatSticks10iOS
+//
+//  Created by Nick Rodriguez on 29/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
